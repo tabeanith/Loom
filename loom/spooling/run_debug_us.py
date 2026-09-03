af994e17a8dc25c1352d9b73724cb44b8169bbaa
lmc <l.m.christ@protonmail.com>
2026-09-03 16:54:45 +0200

    ok

--- loom/spooling/run_debug_us.py
+++ loom/spooling/run_debug_us.py
@@ -1,3 +1,4 @@
+import pstats
 
 from loom.spooling.source.severe_weather_europe.run import SevereWeatherEurope
 from loom.spooling.source.theguardian.run import TheGuardian
@@ -19,6 +20,7 @@ from loom.spooling.analyse_scores import calculate_sentiment_v1
 from loom.spooling.analyse_scores import calculate_sentiment_vn
 from loom.spooling.analyse_utils import calculate_mtm_from_buy_sell
 from loom.spooling.analyse_utils import calculate_mtm_from_open_position
+from loom.spooling.analyse_utils import get_bounded_open_volume
 from loom.utils.kernel import numba_rolling_quantile_q_value
 
 
@@ -45,14 +47,15 @@ if __name__ == "__main__":
     if True:
 
         #  ----------------------------------------------  Market prices  ------------------------------------------
-        ts_go = pd.Timestamp(date(2026, 7, 1), tz=tz)
+        ts_go = pd.Timestamp(date(2026, 5, 1), tz=tz)
 
-        symbol = "NQ"
+        symbol = "ES"
         #symbol = "MGC"
         tf = "5min"
         df = read_dataframe(symbol, tf)
         _df = df[df.index > ts_go]
         prices = _df["open"].resample("h").first()
+        prices = _df["open"]#.resample("h").first()
 
 
         #  ----------------------------------------------  Market sentiment  ---------------------------------------
@@ -72,20 +75,36 @@ if __name__ == "__main__":
 
         #  ----------------------------------------------  Strats  -------------------------------------------------
 
-        pricesQ = numba_rolling_quantile_q_value(prices.to_numpy(dtype=np.float32), 24*5)
+        pricesQ = numba_rolling_quantile_q_value(prices.to_numpy(dtype=np.float32), 12*24*3)
         pricesQ = pd.Series(index=prices.index, data=pricesQ)
 
-        buys = (sentiment < 0) & (pricesQ < 0.3)
-        sells = (sentiment < 0) & (pricesQ > 0.7)
+        buys = (sentiment < 0) & (pricesQ < 0.4)
+        sells = ((sentiment < 0) & (pricesQ > 0.6))
 
-        buys = (pricesQ < 0.3)
-        sells = (pricesQ > 0.7)
-        _mtm, _open_volume = calculate_mtm_from_buy_sell(buys, sells, prices)
-        _open_volume = (_open_volume / 10.).astype(int)
-
+        mtm, open_volume = calculate_mtm_from_buy_sell(buys, sells, prices)
+        _open_volume = (open_volume / 10.).astype(int) * 2.
         mtm, open_volume = calculate_mtm_from_open_position(_open_volume, prices)
 
-        print(mtm.iloc[-1])
+
+        # Check boundaries on open_volume and adjust trading:
+        roof = 12.
+        _idx_sentiment = sentiment / 100.
+        maximum =   _idx_sentiment
+        minimum =   _idx_sentiment
+        ind_sentiment = _idx_sentiment * roof
+        _minimum = ind_sentiment #+ minimum
+        _maximum = ind_sentiment #+ maximum
+        _minimum = _minimum.clip(upper=0).clip(lower=-roof)
+        _maximum = _maximum.clip(lower=0).clip(upper=+roof)
+        _maximum = pd.Series(index=sentiment.index, data=200)
+        _minimum = _maximum * -1.
+
+        open_volume_bounded = get_bounded_open_volume(open_volume, _maximum, _minimum)
+        mtm, open_volume = calculate_mtm_from_open_position(open_volume_bounded / 200. * 10., prices)
+
+        print("mtm", mtm.iloc[-1])
+        print("pips trading", mtm.iloc[-1] / open_volume[open_volume != 0].abs().mean())
+        print("pips market", prices.iloc[-1] - prices.iloc[0])
 
 
         #  ----------------------------------------------  Plot  -------------------------------------------------
@@ -102,8 +121,12 @@ if __name__ == "__main__":
         ax2.axhline(y=0, color='black', linewidth=0.8, linestyle='-')
 
         open_volume.plot(ax=ax2, label="open_volume", color="blue")
+        _maximum.plot(ax=ax2, label="mtm", color="blue", linestyle='--')
+        _minimum.plot(ax=ax2, label="mtm", color="blue", linestyle='--')
+
 
         mtm.plot(ax=ax3, label="mtm", color="black")
+
 
 
         plt.legend()
