0358b22927370380449015afb32601596adcf937
lmc <l.m.christ@protonmail.com>
2026-09-04 14:57:28 +0200

    ok

--- loom/spooling/run_debug_us.py
+++ loom/spooling/run_debug_us.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     if True:
 
         #  ----------------------------------------------  Market prices  ------------------------------------------
-        ts_go = pd.Timestamp(date(2026, 8, 1), tz=tz)
+        ts_go = pd.Timestamp(date(2026, 8, 20), tz=tz)
 
         symbol = "ES"
         #symbol = "MGC"
@@ -76,18 +76,16 @@ if __name__ == "__main__":
         scoreH.plot(label = "scoreH")
         sentimentH.plot(label = "sentimentH")
 
-        df_result["score"].rolling(12).mean().plot()
-        df_result["sentiment"].rolling(12).mean().plot()
-        scoreH.rolling(12).mean().plot(label="scoreH mean")
-        sentimentH.rolling(12).mean().plot(label="sentimentH mean")
+        fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
+        _df["open"].plot(ax=ax1, color="black", label="market")
+
+        scoreH.rolling(12).mean().plot(ax=ax2, label="scoreH mean")
+        sentimentH.rolling(12).mean().plot(ax=ax2, label="sentimentH mean")
+        df_result["score"].rolling(6).mean().plot(ax=ax2)
+        df_result["sentiment"].rolling(6).mean().plot(ax=ax2)
         plt.legend()
         plt.show()
 
-
-        sentiment = calculate_sentiment_v1(df_result, prices, 7)
-        sentiment_bull = calculate_sentiment_v1(df_result[df_result["score"] > 0], prices,  7)
-        sentiment_bear = calculate_sentiment_v1(df_result[df_result["score"] < 0], prices,  7)
-        sentiment_mkt = sentiment_bull.reindex(sentiment.index).ffill().diff() + sentiment_bear.reindex(sentiment.index).ffill().diff()
 
 
         #  ----------------------------------------------  Strats  -------------------------------------------------
@@ -95,7 +93,7 @@ if __name__ == "__main__":
         pricesQ = numba_rolling_quantile_q_value(prices.to_numpy(dtype=np.float32), 24*2)
         pricesQ = pd.Series(index=prices.index, data=pricesQ)
 
-        _maximum = pd.Series(index=sentiment.index, data=200)
+        _maximum = pd.Series(index=prices.index, data=200)
         _minimum = _maximum * -1.
 
         buys = ( (pricesQ < 0.3)).astype(int) * 1
@@ -108,20 +106,22 @@ if __name__ == "__main__":
 
 
 
-        buys1 = sentiment_bull.diff().clip(lower=0)
-        sells1 = sentiment_bear.diff().clip(upper=0) * -1.
+        buys1 = sentimentH.diff().clip(lower=0)
+        sells1 = sentimentH.diff().clip(upper=0) * -1.
         mtm, open_volume = calculate_mtm_from_buy_sell(buys1, sells1, prices)
-        _open_volume = (open_volume / 1.).astype(int) * 5.
-        open_volume_bounded = get_bounded_open_volume(_open_volume, _maximum, _minimum)
-        mtm2, open_volume2 = calculate_mtm_from_open_position(open_volume_bounded / 250. * 10., prices)
+        #_open_volume = (open_volume / 1.).astype(int) * 5.
+        open_volume_bounded = get_bounded_open_volume(open_volume, _maximum, _minimum)
+        mtm2, open_volume2 = calculate_mtm_from_open_position(open_volume_bounded, prices)
 
 
 
 
 
 
-        result_mtm = mtm1  #+ mtm2
-        result_open_volume = open_volume1 #+ open_volume2
+        result_mtm = mtm1  + mtm2
+        result_mtm = mtm2
+        result_open_volume = open_volume1  + open_volume2
+        result_open_volume =  open_volume2
         print("mtm", result_mtm.iloc[-1])
         print("pips trading", result_mtm.iloc[-1] / result_open_volume[result_open_volume != 0].abs().mean())
         print("pips market", prices.iloc[-1] - prices.iloc[0])
@@ -135,9 +135,9 @@ if __name__ == "__main__":
         prices.plot(ax=ax1, label="open", color="black")
         ax2.scatter(x=scores.index, y=scores.values, label="scores", marker='o', linestyle='None')
         ax2.stem(scores.index, scores.values)
-        sentiment_bull.plot(ax=ax2, label="sentiment_bull", color="green")
-        (-sentiment_bear).plot(ax=ax2, label="sentiment_bear", color="red")
-        sentiment.plot(ax=ax2, label="sentiment", color="orange")
+        #sentiment_bull.plot(ax=ax2, label="sentiment_bull", color="green")
+       # (-sentiment_bear).plot(ax=ax2, label="sentiment_bear", color="red")
+        #sentiment.plot(ax=ax2, label="sentiment", color="orange")
         ax2.axhline(y=0, color='black', linewidth=0.8, linestyle='-')
 
         _maximum.plot(ax=ax2, label="mtm", color="blue", linestyle='--')
