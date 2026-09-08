ac000fb2fa9e82e69fe01be857522a746520a225
lmc <l.m.christ@protonmail.com>
2026-09-08 22:51:35 +0200

    ok

--- loom/spooling/analyse_ta_M_Q.py
+++ loom/spooling/analyse_ta_M_Q.py
@@ -297,7 +297,7 @@ if __name__ == "__main__":
 
 
     contract_sampling = "MS"
-    start_n_month_before_del = 3
+    start_n_month_before_del = 6
     hours = 24 * 30
     mw_sizing = 5
     mw_maximum = 50
