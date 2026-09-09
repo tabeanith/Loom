11e6fa0b6ef0e2aac4b4af7e481dfc664404d96c
Christ Lena <l.christ@enbw.com>
2026-09-09 15:44:39 +0200

    ok

--- loom/spooling/analyse_ta_M_Q.py
+++ loom/spooling/analyse_ta_M_Q.py
@@ -299,8 +299,8 @@ if __name__ == "__main__":
     contract_sampling = "MS"
     start_n_month_before_del = 6
     hours = 24 * 30
-    mw_sizing = 5
-    mw_maximum = 50
+    mw_sizing = 4
+    mw_maximum = 40
     total_mtm_months, df_all_open_volume_months = run(contract_sampling, start_n_month_before_del, hours, mw_sizing, mw_maximum)
 
 
